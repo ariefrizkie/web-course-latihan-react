22edd4275d48c9da8d4fc1b297a38866a9eb7a2d
Arif Rizki <arifwise67@gmail.com>
2024-02-04 13:56:43 +0800

    latihan React multipage website

---
+++ src/pages/SyaratPage.jsx
@@ -0,0 +1,137 @@
+import { Container, Row, Col } from "react-bootstrap";
+import FaqComponents from "../components/FaqComponents";
+
+const SyaratPage = () => {
+  return (
+    <div className="syarat-ketentuan-page">
+      <div className="syarat-ketentuan min-vh-100">
+        <Container>
+          <Row>
+            <Col>
+              <h1 className="text-center fw-bold mb-2 animate__animated animate__fadeInUp animate__delay-1s">
+                Syarat & Ketentuan
+              </h1>
+              <p className="text-center animate__animated animate__fadeInUp animate__delay-1s">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              </p>
+            </Col>
+          </Row>
+          <Row className="pt-5">
+            <Col>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Officiis minima voluptatibus culpa perferendis quos saepe eum
+                qui quaerat cumque. Deserunt, itaque quam vero quaerat,
+                consequatur unde expedita debitis beatae, similique nulla
+                perspiciatis veritatis placeat vitae! Magni iure voluptatibus
+                sapiente repudiandae deleniti quisquam aliquid commodi?
+                Cupiditate architecto eaque vel velit repudiandae harum officia,
+                aliquid cumque rem quis obcaecati itaque, a sequi sint fugiat
+                possimus, dignissimos expedita sapiente aut. Vel, aut nemo!
+              </p>
+            </Col>
+          </Row>
+          <Row className="py-3">
+            <Col>
+              <h4 className="fw-bold">1. Lorem</h4>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Pariatur debitis autem, sapiente est corporis eius non officiis
+                deserunt quibusdam modi laudantium vero eos porro cupiditate
+                quaerat vel consectetur ab architecto error nihil et
+                consequatur! Adipisci, quibusdam et? Blanditiis, magnam officia!
+              </p>
+              <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Facilis repellendus voluptates consequuntur repellat deserunt,
+                ipsam magnam quisquam ut ipsa accusamus explicabo velit odio
+                repudiandae optio modi quasi natus voluptas aperiam nemo est nam
+                ipsum commodi quos rem! Deleniti, dolorum molestiae.
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos
+                minima itaque earum qui, asperiores commodi autem hic tempora
+                laborum ipsum fugiat enim, cumque nisi odio? Magnam debitis
+                aperiam odio beatae molestias quo? Quo distinctio eveniet ut
+                nobis sunt, obcaecati numquam doloribus nihil asperiores
+                mollitia corporis ipsa illum iure consequatur ipsum et tempore
+                ex. Quibusdam iste similique architecto aliquid debitis nemo
+                dolore quasi vero recusandae odit magnam non ex, ad placeat
+                autem, voluptatibus commodi suscipit maiores, exercitationem eos
+                officia molestias reprehenderit?
+              </p>
+            </Col>
+          </Row>
+          <Row className="py-3">
+            <Col>
+              <h4 className="fw-bold">2. Lorem</h4>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Possimus ipsa excepturi voluptatibus hic labore neque suscipit
+                ea animi veritatis corporis porro natus ipsum quisquam quis,
+                reprehenderit commodi? Odit et sequi perferendis vel officiis
+                minus consequuntur. Adipisci a nesciunt quaerat ex unde quos
+                laborum error quasi qui? Culpa temporibus sunt alias obcaecati
+                incidunt, molestias, expedita facere ipsam facilis officia cum
+                id?
+              </p>
+              <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Consequuntur praesentium sint perferendis aspernatur nostrum hic
+                accusamus quae ea repellendus itaque aliquam ut modi adipisci,
+                vel quos placeat ipsum eveniet quod inventore! Ullam, ea
+                sapiente? Suscipit ducimus culpa ratione amet, placeat veritatis
+                tenetur deleniti labore facere consectetur, minima soluta nobis
+                sequi asperiores magnam iure, commodi hic in consequatur?
+                Necessitatibus temporibus dolore eos eligendi dolores! Libero,
+                quibusdam accusamus ab facilis inventore modi?
+              </p>
+            </Col>
+          </Row>
+          <Row className="py-3">
+            <Col>
+              <h4 className="fw-bold">3. Lorem</h4>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Possimus ipsa excepturi voluptatibus hic labore neque suscipit
+                ea animi veritatis corporis porro natus ipsum quisquam quis,
+                reprehenderit commodi? Odit et sequi perferendis vel officiis
+                minus consequuntur. Adipisci a nesciunt quaerat ex unde quos
+                laborum error quasi qui? Culpa temporibus sunt alias obcaecati
+                incidunt, molestias, expedita facere ipsam facilis officia cum
+                id?
+              </p>
+              <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima
+                quae, molestiae doloribus similique cupiditate ea amet nisi
+                corrupti quas blanditiis. Veritatis nisi veniam eum modi quia
+                doloribus, aliquid fuga repellendus amet facere qui doloremque
+                repudiandae, eveniet voluptate dolores commodi iste esse
+                expedita debitis officiis accusamus. Commodi laudantium dolore
+                magnam deleniti. Eum suscipit sed rem temporibus molestiae
+                inventore perferendis labore consequuntur modi sint ut deserunt
+                libero voluptatibus at quae vero ex, animi, blanditiis itaque
+                aut repellendus voluptatum, voluptate expedita. Veritatis,
+                dolores.
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Pariatur perspiciatis sapiente numquam fugit nulla consequuntur
+                soluta porro quaerat quos possimus iusto accusantium adipisci
+                magni quis quae, illum natus! Iste, autem ducimus! Asperiores,
+                dolores laborum! Odit quis optio labore similique tenetur
+                blanditiis distinctio quisquam rem voluptatibus provident
+                exercitationem eligendi ullam dolor totam architecto, corrupti
+                molestiae laborum? Assumenda dicta eveniet ea mollitia?
+              </p>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+
+      <FaqComponents />
+    </div>
+  );
+};
+
+export default SyaratPage;
